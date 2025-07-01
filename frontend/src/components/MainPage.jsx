03df53c21d07c21b4227ec90ef37f698f047e27e
Khushi <k23verma@gmail.com>
2025-07-01 21:53:57 +0530

    Built main summarizing section

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -5,9 +5,26 @@ import Header from "./Header";
 import '../styles/summaraize.css';
 
 export default function MainPage() {
+    const[text,setText]=useState('');
+
     return (
         <>
         <Header />
+        <div className="begin">
+            <div className="text-input">
+                <textarea  value={text} onChange={(c)=> setText(c.target.value)} placeholder="Enter or paste your text here" maxLength={4000}></textarea>
+                <div className="char-count">{text.length}/4000 characters</div>
+            </div>
+            <div className="summary-modes">
+                <label><input type="radio" name="mode" className="mode-btn" value="academic" />Academic</label>
+                <label><input type="radio" name="mode" className="mode-btn" value="friendly" />Friendly</label>
+                <label><input type="radio" name="mode" className="mode-btn" value="formal" />Formal</label>
+                <label><input type="radio" name="mode" className="mode-btn" value="professional" />Professional</label>
+            </div>
+            <button className="summaraize">SUMMARAIZE</button>
+        </div>
+        <div className="how-to"></div>
+        <div className="features"></div>
         <Footer />
         </>
     );
