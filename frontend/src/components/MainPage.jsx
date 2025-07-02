94c4c8aef5d3422cbb9ccc23c94c482dc62507ce
Khushi <k23verma@gmail.com>
2025-07-02 21:23:45 +0530

    Built the how-to and features section UI

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 import '../styles/summaraize.css';
+import p1 from '../assets/Frame 1.png';
+import p2 from '../assets/Frame 2.png';
+import p3 from '../assets/Frame 3.png';
 
 export default function MainPage() {
     const[text,setText]=useState('');
@@ -27,8 +30,54 @@ export default function MainPage() {
             </div>
             <button className="summaraize">SUMMARAIZE</button>
         </div>
-        <div className="how-to"></div>
-        <div className="features"></div>
+        <div className="summary">
+            <div className="summary-output">
+                <h2>Summary</h2>
+                <button className="copy">COPY</button>
+            </div>
+        </div>
+        <div className="how-to">
+            <h2>How to use</h2>
+            <p>Using summarAIze is super easy. Just follow these steps:</p>
+            <div className="how-to-points">
+                <div className="points">
+                    <img src={p1}/>
+                    <p>Type or paste your text into the box above.</p>
+                </div>
+                <div className="points">
+                    <img src={p2}/>
+                    <p>Choose a mode (Academic, Friendly, Formal, Professional).</p>
+                </div>
+                <div className="points">
+                    <img src={p3}/>
+                    <p>Click the <span>Summaraize</span> button and sit tight.</p>
+                </div>
+            </div>
+        </div>
+        <div className="features">
+            <h2>Features</h2>
+            <div className="feature">
+                <div className="f-title">
+                    <h2></h2>
+                    <img />
+                </div>
+                <p></p>
+            </div>
+            <div className="feature">
+                <div className="f-title">
+                    <h2></h2>
+                    <img />
+                </div>
+                <p></p>
+            </div>
+            <div className="feature">
+                <div className="f-title">
+                    <h2></h2>
+                    <img />
+                </div>
+                <p></p>
+            </div>
+        </div>
         <Footer />
         </>
     );
