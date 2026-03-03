02ca47378a467872ce6b6ce71db8f65f88ede9d1
Khushi Verma <168122745+Khushi23V@users.noreply.github.com>
2026-03-03 20:14:35 +0530

    Update ai.service.js

--- backend/services/ai.service.js
+++ backend/services/ai.service.js
@@ -5,7 +5,7 @@ async function generateSummary({ prompt }) {
     const response = await axios.post(
       "https://api.groq.com/openai/v1/chat/completions",
       {
-        model: "llama3-8b-8192",
+        model: "llama-3.1-8b-instant",
         messages: [
           {
             role: "system",
@@ -36,3 +36,4 @@ async function generateSummary({ prompt }) {
 }
 
 module.exports = { generateSummary };
+
