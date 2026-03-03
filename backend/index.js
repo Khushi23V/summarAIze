92eb69643a7f06708151ed79b6eab39f87c6eb62
Khushi Verma <168122745+Khushi23V@users.noreply.github.com>
2026-03-03 19:16:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.post("/summarize", async (req, res) => {
           Authorization: `Bearer ${HF_API_KEY}`,
           "Content-Type": "application/json",
         },
-        timeout: 20000
+        timeout: 60000
       }
     );
 
