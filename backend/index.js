0794f959532ab2aae4ff3e176d362ce8537f5dcc
Khushi Verma <168122745+Khushi23V@users.noreply.github.com>
2026-03-04 15:01:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,13 @@ const cors = require("cors");
 
 const { PORT } = require("./config/env");
 const summarizeRoute = require("./routes/summarize.route");
+const urlRoute = require("./routes/url.route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/summarize-url", urlRoute);
 app.use("/summarize", summarizeRoute);
 
 app.listen(PORT, () => {
